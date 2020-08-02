378fac45f3430d6b2af4dcae31fe515909b316e1
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-02 05:14:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,37 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
+<<<<<<< HEAD
 	monopoly = {:railroads=> { :pieces=>4, :names=>{ :reading_railroad=> {}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline=>{}}, :rent_in_dollars=>{ :one_piece_owned=>25, :two_pieces_owned=>50, :three_pieces_owned=>100, :four_pieces_owned=>200} } }
+=======
+  monopoly =  {
+  :railroads=>
+  {
+    :pieces=>4,
+    :rent_in_dollars=>
+    {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    },
+    :names=>
+    {
+      :reading_railroad=>
+      {
+      },
+      :pennsylvania_railroad=>
+      {
+      },
+      :b_and_o_railroad=>
+      {
+      },
+      :shortline=>
+      {
+      }
+    }
+  }
+>>>>>>> 93be82c32821bc9b9f0c1f4b8b2312d0d5220861
 end
 
 def monopoly_with_fourth_tier
