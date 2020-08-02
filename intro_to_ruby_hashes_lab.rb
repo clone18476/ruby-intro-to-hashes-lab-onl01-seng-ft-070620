bbf1abdab0877328827a438f96baf237f16de81a
Cameron Cousins <cameroncousins18@gmail.com>
2020-08-02 05:37:07 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -18,7 +18,8 @@ end
 def monopoly_with_third_tier
 <<<<<<< HEAD
 	monopoly = {:railroads=> { :pieces=>4, :names=>{ :reading_railroad=> {}, :pennsylvania_railroad=>{}, :b_and_o_railroad=>{}, :shortline=>{}}, :rent_in_dollars=>{ :one_piece_owned=>25, :two_pieces_owned=>50, :three_pieces_owned=>100, :four_pieces_owned=>200} } }
-	# 
+	# added four hashes to names value
+	# added four hashes with values for rent of owning different number of railroads
 =======
   monopoly =  {
   :railroads=>
